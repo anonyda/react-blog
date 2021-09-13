28f86993b295968d1709fcba7feaadfefbdac241
Nida <zaynida123@gmail.com>
2021-09-13 13:13:19 +0530

    created home page component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import NavBar from '../../components/NavBar/NavBar'
+
+import Banner from '../../components/Banner/Banner'
+import Blogs from '../../components/Blogs/Blogs'
+import styles from './Home.module.css';
+import Footer from '../../components/Footer/Footer';
+
+export default function Home() {
+    return (
+        <div className={styles.home}>
+            <NavBar />
+            <Banner />
+            <Blogs />
+            <Footer />
+        </div>
+    )
+}
