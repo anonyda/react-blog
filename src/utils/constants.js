e01c569381b4cde3c9188bc097c907a6473f5615
Nida Shaikh <zaynida123@gmail.com>
2021-09-27 12:17:31 +0530

    fixed loginServer link

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 // export const loginServerURL = 'http://localhost:5000/users/';
-export const loginServerURL = 'https://authentication-servicev2.herokuapp.com/users/';
+export const loginServerURL = 'https://authentication-servicev2.herokuapp.com/users';
 // export const localBlogServerURL = 'http://localhost:4000/blogs';
 export const localBlogServerURL = 'https://blog-backend-express.herokuapp.com/blogs';
 export const SERVER_ERROR = 'The Server is down.';
